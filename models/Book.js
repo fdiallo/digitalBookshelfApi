fc0ffeb948e863ae2ca9561736896e9f844665d0
fdiallo <fallaye.diallo@gmail.com>
2026-05-18 23:34:22 -0400

    Task 3: Book Schema and Model

---
+++ models/Book.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+const bookSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    author: { type: String, required: true },
+    isbn: { type: String, unique: true },
+    publishedDate: { type: Date, default: Date.now },
+    inStock: { type: Boolean, default: true }
+})
+
+
+const Book = mongoose.Model("Book", bookSchema)
+
+module.exports = Book
